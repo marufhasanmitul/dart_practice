6a023544aa7fe6fd192e81c74d35780e4ea80c7e
Maruf Hasan <marufhasanmitul@gmail.com>
2023-04-14 01:52:09 +0600

    add

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -2,6 +2,8 @@ main(){
   String name="Maruf Hasan";
   //use Integer
   int accountHolderName=45;
+  ///Address
+  String address="Natore,Bagatipara";
   double totalAmount=1000;
   print(accountHolderName);
   print(totalAmount);
