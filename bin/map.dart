3a69d942bfffb2a6599d9f62c10d68f826c906b4
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-04-16 22:01:39 +0600

    Create map.dart
    Literal Map

---
+++ bin/map.dart
@@ -0,0 +1,15 @@
+//==============Map create Literial
+  Map<String,String> myMap={
+   'name':'Maruf Hasan',
+   'age':'28',
+   'dep':'computer',
+   'roll':'201'
+  };
+
+  //Runtime Data add
+  myMap['son']='Salif';
+  myMap['sonage']='2 month';
+
+  print(myMap);
+  print(myMap['name']);
+//==========================================
