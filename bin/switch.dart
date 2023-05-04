363544d3933ab850991569a7978ba426128a0198
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-04 20:06:43 +0600

    Create switch.dart

---
+++ bin/switch.dart
@@ -0,0 +1,20 @@
+ //switch case
+  int m =10;
+
+  switch(m){
+    case 1:
+      print("Value is 1");
+      break;
+    case 2:
+      print("Value is 2");
+      break;
+    case 3:
+      print("Value is 3");
+      break;
+    case 4:
+      print("Value is 4");
+      break;
+    default :
+      print("Out of Range");
+      break;
+  }
