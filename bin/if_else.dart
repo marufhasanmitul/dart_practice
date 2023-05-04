27c77991b0f689343d4e6ed93afea9ed4220d9b6
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-04 20:06:16 +0600

    Create if_else.dart

---
+++ bin/if_else.dart
@@ -0,0 +1,28 @@
+//Dart Decition Making Statements
+
+  var n=35;
+  var y=50;
+  if(n<40){
+    print("The Number smaller than 40");
+  }
+  
+  if(n>y){
+    print("x is grather than y");
+  }else{
+    print("Y is Big");
+  }
+
+  //==================================
+  var markes= 86;
+  if(markes>85){
+    print("Excellent");
+  }
+  else if(markes>75){
+    print("Very Good");
+  }
+  else if(markes>65){
+    print("Good");
+  }
+  else{
+    print("Average");
+  }
