38fe304a2326faf07f35fff8b3a9740594259fc5
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-06 21:21:15 +0600

    Update for_loop.dart

--- bin/for_loop.dart
+++ bin/for_loop.dart
@@ -3,5 +3,31 @@ void main(){
   for(int x=0; x<=100; x=x+2){
     print("Dart "+x.toString() );
   }
+ 
+ //==========================================
+   // kotobar er jonno jabo?
+  // count koto theke shuru korbo?
+  // jete ashle koto bar count barabe?
+  // starting(once) ; condition ; increment/decrement
+
+  for(int start=0; start<100;start=start+1 ){
+    print("Nise Jabe : $start");
+  }
+
+  String name='Rahim';
+  int age=35;
+  String informaton='Name is :$name Age is : $age';//String interpolation
+  print(informaton);
+
+  //Create a List
+  List<String> students=['Rafat','Mehedi','Imaran'];
+  for(int index=0;index<students.length;index=index+1 ){
+    print('Good Maornig ${students[index] }');
+  }
+
+  for(String name in students){
+    print('Good Evening $name ');
+  }
+
 
 }
