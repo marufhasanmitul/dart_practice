9aed3b4d5b2833ad893ac36ade270d429b167109
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-08 22:38:11 +0600

    Update assinement2.dart

--- assinement2.dart
+++ assinement2.dart
@@ -1,5 +1,51 @@
 void main(){
 
+  String studentName= 'Maruf Hasan';
+
+  int testScore = 85;
+
+
+  studentGrade(studentName,testScore);
+
+
+
+}
+
+void studentGrade(String studentName, int testScore){
+
+  if(testScore>=90 && testScore<=100){
+    print('$studentName\n Grade : A');
+  }
+
+  else if(testScore>=80 && testScore<=89){
+    print('$studentName \n Grade : B');
+
+  }
+  else if(testScore>=70 && testScore<=79){
+    print('$studentName \n Grade : C');
+
+  }
+  else if(testScore>=60 && testScore<=69){
+    print('$studentName \n Grade : D');
+
+  }
+  else if(testScore>=0&& testScore<=59){
+    print('$studentName \n Grade : F');
+
+  }
+
+  else{
+    print("Invalid Grade");
+  }
+
+}
+
+
+
+//=========================Solution 2=====================================
+
+void main(){
+
   String studentName = "Maruf Hasan";
   int testScore = 85;
   String grade = studentGrade(studentName, testScore);
