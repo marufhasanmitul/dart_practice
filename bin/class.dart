a0c871cab668536dfd9ff7370e462e906b6f95c4
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-10 00:45:38 +0600

    Update class.dart

--- bin/class.dart
+++ bin/class.dart
@@ -22,3 +22,81 @@ class myClass{
 
 
 }
+//================================================================
+//================================================================
+
+void main(){
+  Bicycle bicycle=Bicycle();//Instance or Object
+  bicycle.color="Red";
+  bicycle.size=26;
+  bicycle.currentSpeed=0;
+  bicycle.changeGear(10);
+  bicycle.display();
+
+  Animal animal=Animal();
+  animal.name="The Cow";
+  animal.numberofLeg=4;
+  animal.lifeSpan=10;
+  animal.display();
+
+
+
+}
+
+class Animal{
+  //Propertis filds
+  //Methods Filds
+  String? name;
+  int? numberofLeg;
+  int? lifeSpan;
+
+  void display(){
+    print("Animal Name is : $name");
+    print("Number of the leg $numberofLeg");
+    print("Life Span $lifeSpan");
+  }
+
+}
+
+class Area{
+  double? length;
+  double? bredth;
+
+  double calculateArea(){
+    return length! * bredth! ;
+
+  }
+}
+
+class Student{
+  String? name;
+  int? age;
+  int? grade;
+
+  void displayInfo(){
+    print("Student Name $name");
+    print("Student Name $age");
+    print("Student Name $grade");
+  }
+
+}
+
+class Bicycle{
+  late String color;
+  late int size;
+  late int currentSpeed;
+
+  void changeGear(int newValue){
+    currentSpeed=newValue;
+  }
+
+  void display(){
+    print("Color : $color");
+    print("Size : $size");
+    print("Current Speed : $currentSpeed");
+  }
+
+
+}
+
+
