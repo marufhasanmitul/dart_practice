0c477379d8d5e6656d83afe1cf8a87e6a0250e37
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-15 23:34:37 +0600

    Update liveTest3.dart

--- liveTest3.dart
+++ liveTest3.dart
@@ -1,1 +1,22 @@
+class Person {
+  late String name;
+  late int age;
+  late String address;
 
+  Person(this.name, this.age, this.address);
+
+  void sayHello() {
+    print("Hello, my name is $name.");
+  }
+
+  int getAgeInMonths() {
+    return age * 12;
+  }
+}
+
+void main() {
+  Person person = Person("Maruf Hasan", 25, "Natore,Bagatipara");
+  person.sayHello();
+  int ageInMonths = person.getAgeInMonths();
+  print("Age in months: $ageInMonths");
+}
