c68d7a86516a30f45aded6956a463c20bff17079
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-05-22 20:46:54 +0600

    Update assignment4.dart

--- assignment4.dart
+++ assignment4.dart
@@ -6,9 +6,9 @@ abstract class Account{
 
 
   void deposit(double amount){
-        balance=balance+amount;
-        print("Deposite : $amount Tk");
-        print("New balance : $balance Tk");
+    balance=balance+amount;
+    print("Deposite : $amount Tk");
+    print("New balance : $balance Tk");
   }
   //abstract method
   void withdraw(double amount);
@@ -42,36 +42,37 @@ class CurrentAccount extends Account{
   late double overdraftLimit;
 
   CurrentAccount(int accountNumber,double balance,this.overdraftLimit):super(accountNumber,balance);
-  
+
   @override
   void withdraw(double amount) {
     ///withdraw Limitation
-   if(amount<=overdraftLimit){
-     balance=balance-amount;
-     print("Withdraw : $amount ");
-     print("New balance with interest : $balance Tk");
-   }else{
-     print("Insufficient funds. Cannot withdraw $amount");
-   }
-   
+    if(amount<=overdraftLimit){
+      balance=balance-amount;
+      print("Withdraw : $amount ");
+      print("New balance with interest : $balance Tk");
+    }else{
+      print("Insufficient funds. Cannot withdraw $amount");
+    }
+
   }
 }
 
 void main(){
-  //Create an instance of the SavingsAccount class by providing values for the account
-  // number, initial balance, and interest rate.
+  //Create an instance of the SavingsAccount class by providing values for the
+  //account number, initial balance, and interest rate.
 
-  SavingsAccount savingsAccount=SavingsAccount(2150152,0,16);
-  savingsAccount.deposit(200);
-  savingsAccount.withdraw(100);
+  SavingsAccount savingsAccount=SavingsAccount(2150001,40000,5);
+  savingsAccount.deposit(10000);
+  savingsAccount.withdraw(9000);
   print('\n');
   print('\n');
 
   //Create an instance of the CurrentAccount class by providing values for the
+
   //accountNumber, initial balance, and overdraft limit.
 
-  CurrentAccount currentAccount=CurrentAccount(2150152,0,100);
-  currentAccount.deposit(200);
-  currentAccount.withdraw(101);
+  CurrentAccount currentAccount=CurrentAccount(2150001,50000,20000);
+  currentAccount.deposit(5000);
+  currentAccount.withdraw(4000);
 
 }
